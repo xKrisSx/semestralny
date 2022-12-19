1bf3b17341a0dc420a674c0f799f7f5d607f4ff5
Wiktor <wiktorchowaniec60@gmail.com>
2022-12-20 00:34:03 +0100

    a

--- games/timetest/script.js
+++ games/timetest/script.js
@@ -1,4 +1,4 @@
-let tablica1 = [true, false, true, false];
+let tablica1 = [true, false, true, false, true, false, true, false, true, false, true, false];
 let x = 0;
 let y = 0;
 let z = 0;
@@ -10,12 +10,13 @@ function funkcja(n){
     z = tablica1[n + 1]
     if(!y){
         wynik = new Date().getTime() - date;
-        document.getElementById("dwa").innerHTML = wynik;
-        tablica1.splice(n + 1 ,1,"true")
+        document.getElementById(n).innerHTML = wynik;
+        tablica1.splice(n + 1 ,1,true)
+        tablica1.splice(n,1,true)
+
     } else if(!z) {
         date = new Date().getTime();
-        document.getElementById("jeden").innerHTML = date;
-        tablica1.splice(n,1,"false")
+        tablica1.splice(n,1,false)
     }
 
 
